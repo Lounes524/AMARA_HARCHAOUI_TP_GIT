25671c270a5f18b96ec853a89872852288b5ac03
unknown <jm_harchaoui@esi.dz>
2023-01-08 18:34:24 +0100

    Affichage des réservations

--- src/main/java/com/telly/dao/ReserveDao.java
+++ src/main/java/com/telly/dao/ReserveDao.java
@@ -1,10 +1,14 @@
 package com.telly.dao;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 @Transactional
@@ -20,5 +24,13 @@ public class ReserveDao {
     @Transactional
     public void reserve(Reserve reserve) {
         session().save(reserve);
+    }
+
+
+    @SuppressWarnings("unchecked")
+    public List<Reserve> getReserve(String username) {
+        Criteria crit = session().createCriteria(Reserve.class);
+        crit.add(Restrictions.eq("user.username", username));
+        return crit.list();
     }
 }
